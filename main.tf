57179f0c4d953d9ec4b2c5d41752765645a19bbf
Breno Lipi <brenorlipi@hotmail.com>
2026-04-06 19:47:45 -0300

    refactor: only set up VPC and SG

--- main.tf
+++ main.tf
@@ -2,28 +2,28 @@ provider "aws" {
   region = "us-east-1"
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "app_server" {
-  count         = var.instance_count
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
-
-  vpc_security_group_ids = [module.vpc.default_security_group_id]
-  subnet_id = module.vpc.private_subnets[0]
-  tags = {
-    Name = "${var.instance_name}-${count.index + 1}"
-  }
-}
+#data "aws_ami" "ubuntu" {
+#  most_recent = true
+#
+#  filter {
+#    name   = "name"
+#    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+#  }
+#
+#  owners = ["099720109477"] # Canonical
+#}
+#
+#resource "aws_instance" "app_server" {
+#  count         = var.instance_count
+#  ami           = data.aws_ami.ubuntu.id
+#  instance_type = var.instance_type
+#
+#  vpc_security_group_ids = [module.vpc.default_security_group_id]
+#  subnet_id = module.vpc.private_subnets[0]
+#  tags = {
+#    Name = "${var.instance_name}-${count.index + 1}"
+#  }
+#}
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
@@ -32,9 +32,138 @@ module "vpc" {
   name = "example-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-east-1a", "us-east-1b", "us-west-1c"]
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d", "us-east-1e", "us-east-1f" ]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24"]
 
   enable_dns_hostnames    = true
+}
+
+module "k8s_control_plane_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "~> 5.0"
+
+  name        = "k8s-control-plane-sg"
+  description = "Security group for K8s control plane"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 6443
+      to_port     = 6443
+      protocol    = "tcp"
+      description = "API server"
+      cidr_blocks = "10.0.1.0/24"
+    },
+    {
+      from_port   = 2379
+      to_port     = 2380
+      protocol    = "tcp"
+      description = "etcd server client api"
+      cidr_blocks = "10.0.1.0/24"
+    },
+    {
+      from_port   = 10250
+      to_port     = 10250
+      protocol    = "tcp"
+      description = "Kubelet API"
+      cidr_blocks = "10.0.1.0/24"
+    },
+    {
+      from_port   = 10259
+      to_port     = 10259
+      protocol    = "tcp"
+      description = "kube-scheduler"
+      cidr_blocks = "10.0.1.0/24"
+    },
+    {
+      from_port   = 10257
+      to_port     = 10257
+      protocol    = "tcp"
+      description = "kube-controller-manager"
+      cidr_blocks = "10.0.1.0/24"
+    },
+  ]
+
+  egress_rules = ["all-all"]
+}
+
+module "k8s_data_plane_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "~> 5.0"
+
+  name        = "k8s-data-plane-sg"
+  description = "Security group for K8s data plane (worker nodes)"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 10250
+      to_port     = 10250
+      protocol    = "tcp"
+      description = "Kubelet API"
+      cidr_blocks = "10.0.1.0/24"
+    },
+    {
+      from_port   = 10256
+      to_port     = 10256
+      protocol    = "tcp"
+      description = "kube-proxy"
+      cidr_blocks = "10.0.1.0/24"
+    },
+    {
+      from_port   = 30000
+      to_port     = 32767
+      protocol    = "tcp"
+      description = "NodePort Services"
+      cidr_blocks = "10.0.1.0/24"
+    },
+    {
+      from_port   = 30000
+      to_port     = 32767
+      protocol    = "udp"
+      description = "NodePort Services"
+      cidr_blocks = "10.0.1.0/24"
+    },
+  ]
+
+  egress_rules = ["all-all"]
+}
+
+module "k8s_inbound_ssh" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "~> 5.0"
+
+  name        = "k8s-inbound-ssh-sg"
+  description = "Security group to allow SSH connection to K8s nodes"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      description = "Allow SSH connection"
+      cidr_blocks = "10.0.101.0/24"
+    }
+  ]
+}
+
+module "k8s_outbound_ssh" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "~> 5.0"
+
+  name        = "k8s-outbound-ssh-sg"
+  description = "Security group to allow SSH connection to K8s nodes"
+  vpc_id      = module.vpc.vpc_id
+
+  egress_with_cidr_blocks = [
+    {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      description = "Connect to nodes via SSH"
+      cidr_blocks = "10.0.1.0/24"
+    }
+  ]
 }
